68824732b2674237ba2e5a6d8d867349ca4aa0a5
Leee Jeffries <33576720+leeej84@users.noreply.github.com>
2022-06-09 14:49:28 +0100

    Test Script

--- Test.ps1
+++ Test.ps1
@@ -2,13 +2,16 @@
 $requri = "https://fa-e2evc.azurewebsites.net/api/orchestrators/Orchestration"
 
 #Test to build a new ADC Single Node
-#$body = @{type="new";vmsize="standard";vmname="myctxadc";osversion="121";adctype="single";vnetname="vnet-e2evc";subnetName="subnet1"}  | ConvertTo-Json #standard, large, xlarge - 2016,2019,2022
+$body = @{type="new";vmsize="standard";vmname="myctxadc";osversion="121";adctype="single";vnetname="vnet-e2evc";subnetName="subnet1"}  | ConvertTo-Json #standard, large, xlarge - 2016,2019,2022
+
 #Test to build a new ADC H/A Node
 #$body = @{type="new";vmsize="standard";email="leee.jeffries@leeejeffries.com";version="2016"}  | ConvertTo-Json #standard, large, xlarge - 2016,2019,2022
+
 ##Test to query an ADCs details
 #$body = @{type="query";vmname="myctxadc"}  | ConvertTo-Json #vmname is mandatory
+
 #Test to remove an ADC
-#$body = @{type="remove";vmname="HVGUMDJZOA"}  | ConvertTo-Json #vmname is mandatory
+#$body = @{type="remove";vmname="myctxadc"}  | ConvertTo-Json #vmname is mandatory
 
 #### Block of code to send the web request ####
 try {
